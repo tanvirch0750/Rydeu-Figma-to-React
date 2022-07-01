4ef58665a112e5311befab1837b15fd8080138c9
tanvirch0750 <tanvir0750@yahoo.com>
2022-07-02 01:13:58 +0600

    implemented hero section

--- src/components/hero-section/HeroSection.jsx
+++ src/components/hero-section/HeroSection.jsx
@@ -11,71 +11,76 @@ const HeroSection = () => {
 
   return (
     <section className="hero">
-      <div className="container">
-        <div className="hero-inner">
-          <div className="hero-content">
-            <h1>Private Transfer & Taxi services in Geneva</h1>
-            <p>
-              Book airport transfers, city transfers and hourly disposal
-              services with driver
-            </p>
-          </div>
-          <div className="hero-form">
-            <div className="form-header-tab">
-              <div className="tab-box">
-                <span>One Way</span>
-              </div>
-              <div className="tab-box">
-                <span>Round Trip</span>
-              </div>
-              <div className="tab-box">
-                <span>Hourly</span>
-              </div>
+      <div className="container hero-inner">
+        <div className="hero-content">
+          <h1>Private Transfer & Taxi services in Geneva</h1>
+          <p>
+            Book airport transfers, city transfers and hourly disposal services
+            with driver
+          </p>
+        </div>
+        <div className="hero-form">
+          <div className="form-header-tab">
+            <div className="tab-box tab-box-active">
+              <span>One Way</span>
             </div>
-            <form className="form-container">
+            <div className="tab-box">
+              <span>Round Trip</span>
+            </div>
+            <div className="tab-box">
+              <span>Hourly</span>
+            </div>
+          </div>
+          <form className="form-container">
+            <div className="form-control">
+              <label>
+                <span>From</span>
+              </label>
+              <input type="text" placeholder="(airport, hotel, address)" />
+            </div>
+            <div className="form-control">
+              <label>
+                <span>To</span>
+              </label>
+              <input type="text" placeholder="(airport, hotel, address)" />
+            </div>
+            <div className="form-input-group">
               <div className="form-control">
-                <label>From</label>
-                <input type="text" placeholder="(airport, hotel, address)" />
-              </div>
-              <div className="form-control">
-                <label>To</label>
-                <input type="text" placeholder="(airport, hotel, address)" />
-              </div>
-              <div className="form-input-group">
-                <div className="form-control">
-                  <label>
-                    <img src={iconCalendar} alt="calender-icon" />
-                  </label>
-                  <input type="text" placeholder="Pickup Date" />
-                </div>
-                <div className="form-control">
-                  <label>
-                    <img src={iconCalendar} alt="calendar-icon" />
-                  </label>
-                  <input type="text" placeholder="Pickup time" />
-                </div>
+                <label>
+                  <img src={iconCalendar} alt="calender-icon" />
+                </label>
+                <input type="text" placeholder="Pickup Date" />
               </div>
               <div className="form-control">
                 <label>
-                  <img src={iconEmail} alt="eamil-icon" />
+                  <img src={iconCalendar} alt="calendar-icon" />
                 </label>
-                <input type="text" placeholder="Email" />
+                <input type="text" placeholder="Pickup time" />
               </div>
+            </div>
+            <div className="form-control">
+              <label>
+                <img src={iconEmail} alt="eamil-icon" />
+              </label>
+              <input type="email" placeholder="Email" />
+            </div>
 
-              <div className="form-control">
-                <PhoneInput
-                  placeholder="Phone Number"
-                  defaultCountry="GB"
-                  value={value}
-                  onChange={setValue}
-                />
-              </div>
-              <div className="form-checkbox">
-                <input type="checkbox" />
-                <label>Additional Requirements</label>
-              </div>
-            </form>
-          </div>
+            <div className="form-control">
+              <PhoneInput
+                placeholder="Phone Number"
+                defaultCountry="GB"
+                value={value}
+                onChange={setValue}
+              />
+            </div>
+            <div className="form-checkbox">
+              <input type="checkbox" />
+              <label>Additional Requirements</label>
+            </div>
+            <div className="form-btn">
+              <button type="submit">Search</button>
+            </div>
+          </form>
         </div>
       </div>
     </section>
